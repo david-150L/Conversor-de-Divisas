a51df3181ec69a9b5660fe8a48ff08ba83fbab51
david <davidmarquez007@hotmail.com>
2023-07-09 16:45:26 -0500

    Actualizaciones en dependencias

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,10 @@
 module com.example.conversordivisasfx {
-    requires javafx.controls;
-    requires javafx.fxml;
+
     requires org.json;
     requires org.apache.httpcomponents.httpclient;
     requires org.apache.httpcomponents.httpcore;
+    requires javafx.controls;
+    requires javafx.fxml;
 
     opens com.example.conversordivisasfx to javafx.fxml;
     exports com.example.conversordivisasfx;
