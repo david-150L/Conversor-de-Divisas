ff551ee1c322be6f5d420c8645bd6f86ac440b29
david <davidmarquez007@hotmail.com>
2023-07-10 15:02:24 -0500

    Detalles minimos en la documentacion

--- src/main/java/com/example/conversordivisasfx/APIExchangeRateImplementacion.java
+++ src/main/java/com/example/conversordivisasfx/APIExchangeRateImplementacion.java
@@ -11,17 +11,31 @@ import org.apache.http.util.EntityUtils;
 import java.io.IOException;
 
 
+/**
+ * Implementación de la API ExchangeRate.
+ */
 public class APIExchangeRateImplementacion {
 
     private static CloseableHttpClient httpClient;
 
+    /**
+     * Inicia la conexión con la API de tipo de cambio.
+     */
     public static void iniciarConection(){
         httpClient = HttpClients.createDefault();
     }
 
+    /**
+     * Convierte una cantidad de una divisa base a una divisa de destino haciendo uso
+     * de los servicios de la API ExchangeRate.
+     *
+     * @param base     La divisa base.
+     * @param destino  La divisa de destino.
+     * @param importe  El importe a convertir.
+     * @return El resultado de la conversión, JSON.
+     */
     public static String convertir(String base, String destino, String importe) {
-
-        String endpoint = "https://v6.exchangerate-api.com/v6/7a3736d8401e85227eae59ef" + "/pair/" + base + "/" + destino + "/" + importe;
+        String endpoint = "https://v6.exchangerate-api.com/v6/7a3736d8401e85227eae59ef/pair/" + base + "/" + destino + "/" + importe;
 
         try {
             HttpGet request = new HttpGet(endpoint);
@@ -29,7 +43,6 @@ public class APIExchangeRateImplementacion {
                 int statusCode = response.getStatusLine().getStatusCode();
                 if (statusCode == 200) {
                     HttpEntity entity = response.getEntity();
-                    //System.out.println(responseBody);
                     return EntityUtils.toString(entity);
                 } else {
                     System.out.println("Error en la solicitud. Código de respuesta: " + statusCode);
@@ -43,3 +56,4 @@ public class APIExchangeRateImplementacion {
         return "Error en la conversión";
     }
 }
+
